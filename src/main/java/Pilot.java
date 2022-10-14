c97aa52bba81f233c00c8c97bf9c1a388853536c
David Valdez Taylor <davidvaldeztaylor@gmail.com>
2022-10-14 16:31:07 +0100

    setup pilot tests have passed

--- src/main/java/Pilot.java
+++ src/main/java/Pilot.java
@@ -1,2 +1,12 @@
-public class Pilot {
+public class Pilot extends CrewMember{
+
+    private String licenceNumber;
+    public Pilot(String name, CrewMemberRank rank, String licenceNumber){
+        super(name, rank);
+        this.licenceNumber = licenceNumber;
+    }
+
+    public String getLicenceNumber() {
+        return licenceNumber;
+    }
 }
--- src/test/java/PilotTest.java
+++ src/test/java/PilotTest.java
@@ -1,3 +1,30 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class PilotTest {
+
+    Pilot pilot;
+
+    @Before
+    public void before(){
+        pilot = new Pilot("Randy Savage", CrewMemberRank.CAPTAIN, "007009");
+    }
+
+    @Test
+    public void canGetName(){
+        assertEquals("Randy Savage", pilot.getName());
+    }
+
+    @Test
+    public void canGetRank(){
+        assertEquals(CrewMemberRank.CAPTAIN, pilot.getRank());
+    }
+
+    @Test
+    public void canGetLicenceNumber(){
+        assertEquals("007009", pilot.getLicenceNumber());
+    }
 
 }
