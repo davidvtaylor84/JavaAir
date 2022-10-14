7279169c30261f70ce2fc8809911b280e58f5a3e
David Valdez Taylor <davidvaldeztaylor@gmail.com>
2022-10-14 17:07:26 +0100

    added flight class

--- src/main/java/Flight.java
+++ src/main/java/Flight.java
@@ -1,2 +1,56 @@
+import java.util.ArrayList;
+
 public class Flight {
+
+    private ArrayList<Pilot> pilots;
+    private ArrayList<CabinCrewMember> cabinCrew;
+    private ArrayList<Passenger> passengers;
+    private String flightNumber;
+    private String destination;
+    private String departureLocation;
+    private String departureTime;
+    private int capacity;
+
+    public Flight(String flightNumber, String destination, String departureLocation, String departureTime, int capacity){
+        this.pilots = new ArrayList<Pilot>();
+        this.cabinCrew = new ArrayList<CabinCrewMember>();
+        this.passengers = new ArrayList<Passenger>();
+        this.flightNumber = flightNumber;
+        this.destination = destination;
+        this.departureLocation = departureLocation;
+        this.departureTime = departureTime;
+        this.capacity = capacity;
+    }
+
+    public ArrayList<Pilot> getPilots() {
+        return pilots;
+    }
+
+    public ArrayList<CabinCrewMember> getCabinCrew() {
+        return cabinCrew;
+    }
+
+    public ArrayList<Passenger> getPassengers() {
+        return passengers;
+    }
+
+    public String getFlightNumber() {
+        return flightNumber;
+    }
+
+    public String getDestination() {
+        return destination;
+    }
+
+    public String getDepartureLocation() {
+        return departureLocation;
+    }
+
+    public String getDepartureTime() {
+        return departureTime;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
 }
