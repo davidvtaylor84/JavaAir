4b47f8730dedd2ffe5f0c729e99fbfde0011d00a
David Valdez Taylor <davidvaldeztaylor@gmail.com>
2022-10-15 17:19:57 +0100

    doing flight manager tests

---
+++ src/main/java/FlightManager.java
@@ -0,0 +1,34 @@
+public class FlightManager {
+
+
+    private int totalFlightWeight;
+
+    private int flightCapacity;
+
+    public FlightManager(int totalFlightWeight, int flightCapacity){
+        this.totalFlightWeight = totalFlightWeight;
+        this.flightCapacity = flightCapacity;
+    }
+
+
+    public int getTotalFlightWeight() {
+        return totalFlightWeight;
+    }
+
+    public int getFlightCapacity() {
+        return flightCapacity;
+    }
+
+    public int calculateWeightReservedForLuggage(){
+        return this.totalFlightWeight /2;
+    }
+
+    public int weightOfBagPerPerson(){
+        return calculateWeightReservedForLuggage() / this.flightCapacity;
+    }
+
+    public int calculatePassengersLuggageWeight(int noOfBags){
+        return  weightOfBagPerPerson() * noOfBags;
+    }
+
+}
---
+++ src/test/java/FlightManagerTest.java
@@ -0,0 +1,47 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class FlightManagerTest {
+
+    FlightManager flightManager;
+    Passenger passenger1;
+    Passenger passenger2;
+
+    @Before
+    public void before(){
+        flightManager = new FlightManager(6000, 200);
+        passenger1 = new Passenger("My-Eyes-Are-Bleeding Johnson", 3);
+        passenger2 = new Passenger("Jane Doe", 2);
+    }
+
+    @Test
+    public void canGetTotalFlightWeight(){
+        assertEquals(6000, flightManager.getTotalFlightWeight());
+    }
+
+    @Test
+    public void canGetFlightCapacity(){
+        assertEquals(200, flightManager.getFlightCapacity());
+    }
+
+    @Test
+    public void canCalculateOverallWeightForFlightLuggage(){
+        assertEquals(3000, flightManager.calculateWeightReservedForLuggage());
+    }
+
+    @Test
+    public void canCalculateWeightOfBagPerPerson(){
+        assertEquals(15, flightManager.weightOfBagPerPerson());
+    }
+
+    @Test
+    public void canCalculateWeightOfIndividualPassengersLuggage(){
+        assertEquals(30, flightManager.calculatePassengersLuggageWeight(passenger2.getNoOfBags()));
+    }
+
+    @Test
+    public void 
+
+}
